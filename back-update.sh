0397afda234f8bb62db4d713ff0e88c64c31d322
Сергей Тяпкин <Tyapkin2002@mail.ru>
2023-05-13 07:38:14 +0300

    updated with the structure of backend

--- back-update.sh
+++ back-update.sh
@@ -2,8 +2,8 @@ cd /home/tech-support-backend/
 git fetch --all
 git reset --hard origin/master
 git log --oneline -1
-\cp -rf /home/override_config.json /home/tech-support-backend/config.json
-\cp -rf /home/override_autoAchievementsConfig.json /home/tech-support-backend/autoAchievementsConfig.json
+\cp -rf /home/override_config.json /home/tech-support-backed/configs/config.json
+\cp -rf /home/override_autoAchievementsConfig.json /home/tech-support-backend/configs/autoAchievementsConfig.json
 systemctl daemon-reload
 systemctl restart gunicorn_site
 echo "Backend updated successfully"
--- minimalAccessRights.sql
+++ minimalAccessRights.sql
@@ -2,9 +2,25 @@ REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "tech-support-backend"
 REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM "tech-support-backend";
 REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM "tech-support-backend";
 
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "tech-support-backend";
+SELECT 'ALTER TABLE '|| schemaname || '."' || tablename ||'" OWNER TO "tech-support-backend";'
+FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'information_schema')
+ORDER BY schemaname, tablename;
+-- You must execute all ALTERS that was printed by previous string --
+
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO "tech-support-backend";
+SELECT 'ALTER FUNCTION '
+            || quote_ident(n.nspname) || '.'
+            || quote_ident(p.proname) || '('
+            || pg_catalog.pg_get_function_identity_arguments(p.oid)
+            || ') OWNER TO foo;'
+FROM   pg_catalog.pg_proc p
+JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+WHERE  n.nspname = 'public';
+-- You must execute all ALTERS that was printed by previous string --
+
 GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO "tech-support-backend";
 
--- Now "tech-support-backend" can only SELECT, UPDATE, INSERT, DELETE in all tables
--- And use trigger functions
+-- Now "tech-support-backend" can CREATE, SELECT, UPDATE, INSERT, DELETE all tables
+-- And EXECUTE, CREATE. DROP trigger functions
